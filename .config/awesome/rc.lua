0f335d113d56353da4947dee986bc33e6aff906c
silverscat-3 <silverscat_3@mail.sc3.fun>
2020-07-13 21:48:30 +0900

    Edit something

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "themes/good-old-days/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "termite"
